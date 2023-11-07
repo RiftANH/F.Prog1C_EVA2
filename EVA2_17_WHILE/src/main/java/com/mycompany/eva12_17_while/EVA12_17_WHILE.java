e125c772522f45f635d8135ecde71b650c317cec
RiftANH <143563089+RiftANH@users.noreply.github.com>
2023-11-07 09:51:59 -0700

    Add files via upload

---
+++ EVA2_17_WHILE/src/main/java/com/mycompany/eva12_17_while/EVA12_17_WHILE.java
@@ -0,0 +1,12 @@
+package com.mycompany.eva12_17_while;
+
+public class EVA12_17_WHILE {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<10) {
+            System.out.println("Hello world");
+            i++;
+        }
+    }
+}
